ec3901d4f1501c6d0b8b687dbffefe398898bef8
Patrick Astermann <patrick.astermann01@gmail.com>
2023-11-19 11:56:00 +1100

    Worked on enemy pathing

--- ProjectMax/enemy.cs
+++ ProjectMax/enemy.cs
@@ -3,5 +3,21 @@ using System;
 
 public partial class enemy : CharacterBody3D
 {
-    
+    [Export] public float enemySpeed { get; set;} = 5.1f;
+    private player player;
+
+    public override void _Ready()
+    {
+        player =  GetNode<player>("Player");
+    }
+
+    public override void _Process(double delta)
+    {
+        base._Process(delta);
+    }
+
+    private void FollowPlayer(double delta)
+    {
+        
+    }
 }
